c378a4cd2e8cb77adf691cfce98b8d9428a4f22e
Sonu Sharma <49761259+sonusharma1997@users.noreply.github.com>
2021-11-25 12:06:20 +0000

    Update test.c

--- 3_Implementation/src/test.c
+++ 3_Implementation/src/test.c
@@ -24,28 +24,8 @@ int main()
 void test_update(void) {
 	
 	
-	if (!glfwInit()) {
-		const char* error = NULL;
-		glfwGetError(&error);
-		fprintf(stderr, "failed to init GLFW3: %s\n", error);
-		assert(0==1);
-	}
-	
-	glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-	
-	// ask for an OpenGL 3.3 Core profile
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
-	
+	glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
 	// set flags for monitor details
-	GLFWmonitor* monitor = glfwGetPrimaryMonitor();
-	const GLFWvidmode* mode = glfwGetVideoMode(monitor);
-	glfwWindowHint(GLFW_RED_BITS, mode->redBits);
-	glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
-	glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
-	glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
 	
 	GLFWwindow* window = NULL;
 	
